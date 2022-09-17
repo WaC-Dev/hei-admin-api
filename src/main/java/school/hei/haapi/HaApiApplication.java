7911569a1b6c8125d3ec850dd583bd85feeb1db4
Mitsanta_ANDRIAH <103644537+Mitsanta12@users.noreply.github.com>
2022-09-17 10:50:53 +0300

    feat: event eventparticipant courses

--- src/main/java/school/hei/haapi/HaApiApplication.java
+++ src/main/java/school/hei/haapi/HaApiApplication.java
@@ -1,9 +1,7 @@
 package school.hei.haapi;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-
 @SpringBootApplication
 @EnableScheduling
 public class HaApiApplication {
---
+++ src/main/java/school/hei/haapi/endpoint/rest/mapper/EventMapper.java
@@ -0,0 +1,40 @@
+/**package school.hei.haapi.endpoint.rest.mapper;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+import school.hei.haapi.model.Event;
+import school.hei.haapi.model.Place;
+import school.hei.haapi.service.PlaceService;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Component
+@AllArgsConstructor
+public class EventMapper {
+    private final PlaceService placeService;
+    public Event toRest(school.hei.haapi.endpoint.rest.model.CreateEvent toCreate){
+        Place place = placeService.getPlaceById(toCreate.getIdPlace());
+        return Event.builder()
+                .id(toCreate.getId())
+                .endTime(toCreate.getEndTime())
+                .startTime(toCreate.getStartTime())
+                .eventType(toCreate.getEventType())
+                .place(place)
+                .build();
+    }
+
+    public List<Event> toDomainList(List<school.hei.haapi.endpoint.rest.model.CreateEvent> event){
+        return event.stream().map(this::toRest).collect(Collectors.toUnmodifiableList());
+    }
+
+    public Event toDomain(school.hei.haapi.endpoint.rest.model.Event event){
+        Place place = placeService.getPlaceById(event.getIdPlace());
+        return Event.builder()
+                .id(event.getId())
+                .place(place)
+                .eventType(event.getEventType())
+                .startTime(event.getStartTime())
+                .endTime(event.getEndTime())
+                .build();
+    }
+}*/
